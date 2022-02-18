4167080d553d394d1716981d7e114f7f4d6d1fe3
carlstep <carlstep333@me.com>
2022-02-18 23:06:20 +0400

    testing playground for currency app

--- lib/main.dart
+++ lib/main.dart
@@ -10,101 +10,184 @@ void main() {
           const Positioned(
             top: 70,
             left: 20,
-            child: Icon(Icons.arrow_back, color: Colors.white,),
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
           ),
+          //Text('Bees\'n\'Honey'),
           const Positioned(
             top: 70,
             right: 20,
-            child: Icon(Icons.settings, color: Colors.white,),
+            child: Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
           ),
           Center(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  left: 50, right: 50, bottom: 20, top: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const CircleAvatar(
-                    radius: 70,
-                    backgroundImage: AssetImage('assets/images/profile.jpg'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.grey[800],
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'John Rambo',
-                    style: GoogleFonts.lato(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 24),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    'john@gmail.com',
-                    style: GoogleFonts.lato(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w300,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 16),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.yellow[300],
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    height: 40,
-                    width: 300,
-                    child: Center(
-                      child: Text(
-                        'Upgrade to Premium',
-                        style: GoogleFonts.lato(
-                            fontSize: 18, fontWeight: FontWeight.w400),
+                  child: Column(
+                    children: [
+                      Row(
+                        // contains flag, currency name and country name
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const CircleAvatar(
+                            radius: 30,
+                            backgroundImage: AssetImage(
+                                'assets/images/flag_placeholder.png'),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Currency Name',
+                                  style: GoogleFonts.lato(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 18),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  'Country Name',
+                                  style: GoogleFonts.lato(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w300,
+                                      fontStyle: FontStyle.italic,
+                                      fontSize: 12),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.yellow[200],
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            height: 30,
+                            width: 150,
+                            child: Center(
+                              child: Text(
+                                'Enter a value',
+                                style: GoogleFonts.lato(
+                                    fontSize: 18, fontWeight: FontWeight.w400),
+                              ),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Icon(
+                              Icons.arrow_forward,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  const SizedBox(
-                    height: 20,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const ProfileMenuItem(
+                  currencyName: 'Albanian LEK',
+                  leadingFlag: Image(
+                    image: AssetImage('assets/images/flag_placeholder.png'),
                   ),
-                  const ProfileMenuItem(
-                    text: 'Your order history',
-                    leadingIcon: Icons.shopping_bag_outlined,
-                    arrowShown: true,
+                  share: true,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const ProfileMenuItem(
+                  currencyName: 'British Pound',
+                  leadingFlag: Image(
+                    image: AssetImage('assets/images/flag_placeholder.png'),
                   ),
-                  const SizedBox(
-                    height: 20,
+                  share: true,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const ProfileMenuItem(
+                  currencyName: 'Euro',
+                  leadingFlag: Image(
+                    image: AssetImage('assets/images/flag_placeholder.png'),
                   ),
-                  const ProfileMenuItem(
-                    text: 'Help and Support',
-                    leadingIcon: Icons.help_outline,
-                    arrowShown: true,
+                  share: true,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const ProfileMenuItem(
+                  currencyName: 'Georgian Lari',
+                  leadingFlag: Image(
+                    image: AssetImage('assets/images/flag_placeholder.png'),
                   ),
-                  const SizedBox(
-                    height: 20,
+                  share: false,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const ProfileMenuItem(
+                  currencyName: 'Albanian LEK',
+                  leadingFlag: Image(
+                    image: AssetImage('assets/images/flag_placeholder.png'),
                   ),
-                  const ProfileMenuItem(
-                    text: 'Load gift voucher',
-                    leadingIcon: Icons.card_giftcard_outlined,
-                    arrowShown: true,
+                  share: true,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const ProfileMenuItem(
+                  currencyName: 'British Pound',
+                  leadingFlag: Image(
+                    image: AssetImage('assets/images/flag_placeholder.png'),
                   ),
-                  const SizedBox(
-                    height: 20,
+                  share: true,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const ProfileMenuItem(
+                  currencyName: 'Euro',
+                  leadingFlag: Image(
+                    image: AssetImage('assets/images/flag_placeholder.png'),
                   ),
-                  const ProfileMenuItem(
-                    text: 'Logout',
-                    leadingIcon: Icons.logout_outlined,
-                    arrowShown: false,
+                  share: true,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const ProfileMenuItem(
+                  currencyName: 'Georgian Lari',
+                  leadingFlag: Image(
+                    image: AssetImage('assets/images/flag_placeholder.png'),
                   ),
-                  const SizedBox(
-                    height: 100,
-                  ),
-                ],
-              ),
+                  share: false,
+                ),
+              ],
             ),
           ),
         ],
@@ -116,12 +199,12 @@ void main() {
 class ProfileMenuItem extends StatelessWidget {
   // ignore: use_key_in_widget_constructors
   const ProfileMenuItem(
-      {required this.text,
-      required this.leadingIcon,
-      required this.arrowShown});
-  final String text;
-  final IconData leadingIcon;
-  final bool arrowShown;
+      {required this.currencyName,
+      required this.leadingFlag,
+      required this.share});
+  final String currencyName;
+  final Image leadingFlag;
+  final bool  share;
 
   @override
   Widget build(BuildContext context) {
@@ -138,30 +221,49 @@ class ProfileMenuItem extends StatelessWidget {
             Row(
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(left: 12.0),
-                  child: Icon(
-                    leadingIcon,
-                    color: Colors.white,
+                  padding: const EdgeInsets.only(left: 4.0, right: 8.0),
+                  child: CircleAvatar(
+                    child: Image.asset('assets/images/flag_placeholder.png'),
+                    backgroundColor: Colors.yellow[200],
+                    radius: 20,
                   ),
                 ),
                 const SizedBox(
                   width: 10,
                 ),
-                Text(
-                  text,
-                  style: GoogleFonts.lato(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
+                Container(
+                  width: 120,
+                  child: Text(
+                    currencyName,
+                    style: GoogleFonts.lato(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  width: 70,
+                  child: Text(
+                    '0000',
+                    style: GoogleFonts.ubuntu(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 30,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ],
             ),
-            arrowShown
+            share
                 ? const Padding(
                     padding: EdgeInsets.only(right: 8.0),
                     child: Icon(
-                      Icons.arrow_forward,
+                      Icons.share,
+                      size: 15,
                       color: Colors.white,
                     ),
                   )
